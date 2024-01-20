e114fbb9649598580a89f4f85a6df68987c086b2
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2024-01-20 11:31:25 +0400

    обновил readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 "use client"
 
-import styles from './page.module.css'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 
 import { IconContext } from "react-icons";
-
 import { FaBitcoin } from "react-icons/fa";
 import { FaEthereum } from "react-icons/fa";
 import { SiTether } from "react-icons/si";
 import { FaArrowRightArrowLeft } from "react-icons/fa6";
 
 export default function Home() {
-
-  const ref = useRef(null);
 
   const [btc, setBtc] = useState<number>(0)
   const [eth, setEth] = useState<number>(0)
@@ -23,27 +19,20 @@ export default function Home() {
   const [leftSide, setLeftSide] = useState<string>("BTC")
   const [rightSide, setRightSide] = useState<string>("ETH")
 
-  const [numberLeft, setNumberLeft] = useState<number | string >(0)
-  const [numberRight, setNumberRight] = useState<number | string >(0)
+  const [numberLeft, setNumberLeft] = useState<number | string>(0)
+  const [numberRight, setNumberRight] = useState<number | string>(0)
 
   useEffect(() => {
     fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
-    .then(res => res.json())
-    .then(res => setBtc(res.price))
-  },[])
+      .then(res => res.json())
+      .then(res => setBtc(res.price))
+  }, [])
 
   useEffect(() => {
     fetch('https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT')
-    .then(res => res.json())
-    .then(res => setEth(res.price))
-  },[])
-
-  if (btc) {
-    console.log(btc)
-  }
-  if (eth) {
-    console.log(eth)
-  }
+      .then(res => res.json())
+      .then(res => setEth(res.price))
+  }, [])
 
   const calculate = (value: number, side: string, leftCoin: string, rightCoin: string) => {
 
@@ -52,34 +41,34 @@ export default function Home() {
 
     if (side === 'left') {
       (leftSide === 'BTC' && rightSide === 'BTC') ? setNumberRight(value)
-      : (leftSide === 'BTC' && rightSide === 'ETH') ? setNumberRight(Math.round(((value * btc / eth) + Number.EPSILON) * 100000000) / 100000000)
-      : (leftSide === 'BTC' && rightSide === 'USDT') ? setNumberRight(Math.round(((value * btc) + Number.EPSILON) * 100000000) / 100000000)
-  
-      : (leftSide === 'ETH' && rightSide === 'ETH') ? setNumberRight(value) 
-      : (leftSide === 'ETH' && rightSide === 'BTC') ? setNumberRight(Math.round(((value * eth / btc) + Number.EPSILON) * 100000000) / 100000000)
-      : (leftSide === 'ETH' && rightSide === 'USDT') ? setNumberRight(Math.round(((value * eth) + Number.EPSILON) * 100000000) / 100000000)
-  
-      : (leftSide === 'USDT' && rightSide === 'USDT') ? setNumberRight(value)
-      : (leftSide === 'USDT' && rightSide === 'BTC') ? setNumberRight(Math.round(((value * usdtbtc) + Number.EPSILON) * 100000000) / 100000000)
-      : (leftSide === 'USDT' && rightSide === 'ETH') ? setNumberRight(Math.round(((value * usdteth) + Number.EPSILON) * 100000000) / 100000000)
-  
-    : null
+        : (leftSide === 'BTC' && rightSide === 'ETH') ? setNumberRight(Math.round(((value * btc / eth) + Number.EPSILON) * 100000000) / 100000000)
+          : (leftSide === 'BTC' && rightSide === 'USDT') ? setNumberRight(Math.round(((value * btc) + Number.EPSILON) * 100000000) / 100000000)
+
+            : (leftSide === 'ETH' && rightSide === 'ETH') ? setNumberRight(value)
+              : (leftSide === 'ETH' && rightSide === 'BTC') ? setNumberRight(Math.round(((value * eth / btc) + Number.EPSILON) * 100000000) / 100000000)
+                : (leftSide === 'ETH' && rightSide === 'USDT') ? setNumberRight(Math.round(((value * eth) + Number.EPSILON) * 100000000) / 100000000)
+
+                  : (leftSide === 'USDT' && rightSide === 'USDT') ? setNumberRight(value)
+                    : (leftSide === 'USDT' && rightSide === 'BTC') ? setNumberRight(Math.round(((value * usdtbtc) + Number.EPSILON) * 100000000) / 100000000)
+                      : (leftSide === 'USDT' && rightSide === 'ETH') ? setNumberRight(Math.round(((value * usdteth) + Number.EPSILON) * 100000000) / 100000000)
+
+                        : null
     }
 
     else if (side === 'right') {
-      (leftSide === 'BTC' && rightSide === 'BTC') ? setNumberLeft(value) 
-      : (leftSide === 'BTC' && rightSide === 'ETH') ? setNumberLeft(Math.round(((value * eth / btc) + Number.EPSILON) * 100000000) / 100000000)
-      : (leftSide === 'BTC' && rightSide === 'USDT') ? setNumberLeft(Math.round(((value * usdtbtc) + Number.EPSILON) * 100000000) / 100000000)
-  
-      : (leftSide === 'ETH' && rightSide === 'ETH') ? setNumberLeft(value) 
-      : (leftSide === 'ETH' && rightSide === 'BTC') ? setNumberLeft(Math.round(((value * btc / eth) + Number.EPSILON) * 100000000) / 100000000)
-      : (leftSide === 'ETH' && rightSide === 'USDT') ? setNumberLeft(value * usdteth)
-  
-      : (leftSide === 'USDT' && rightSide === 'USDT') ? setNumberLeft(value)
-      : (leftSide === 'USDT' && rightSide === 'BTC') ? setNumberLeft(Math.round(((value * btc) + Number.EPSILON) * 100000000) / 100000000)
-      : (leftSide === 'USDT' && rightSide === 'ETH') ? setNumberLeft(Math.round(((value * eth) + Number.EPSILON) * 100000000) / 100000000)
-  
-    : null
+      (leftSide === 'BTC' && rightSide === 'BTC') ? setNumberLeft(value)
+        : (leftSide === 'BTC' && rightSide === 'ETH') ? setNumberLeft(Math.round(((value * eth / btc) + Number.EPSILON) * 100000000) / 100000000)
+          : (leftSide === 'BTC' && rightSide === 'USDT') ? setNumberLeft(Math.round(((value * usdtbtc) + Number.EPSILON) * 100000000) / 100000000)
+
+            : (leftSide === 'ETH' && rightSide === 'ETH') ? setNumberLeft(value)
+              : (leftSide === 'ETH' && rightSide === 'BTC') ? setNumberLeft(Math.round(((value * btc / eth) + Number.EPSILON) * 100000000) / 100000000)
+                : (leftSide === 'ETH' && rightSide === 'USDT') ? setNumberLeft(value * usdteth)
+
+                  : (leftSide === 'USDT' && rightSide === 'USDT') ? setNumberLeft(value)
+                    : (leftSide === 'USDT' && rightSide === 'BTC') ? setNumberLeft(Math.round(((value * btc) + Number.EPSILON) * 100000000) / 100000000)
+                      : (leftSide === 'USDT' && rightSide === 'ETH') ? setNumberLeft(Math.round(((value * eth) + Number.EPSILON) * 100000000) / 100000000)
+
+                        : null
     }
   }
 
@@ -120,7 +109,7 @@ export default function Home() {
         value={{ color: '#f7931a', size: '26px' }}
       >
         <div>
-          <FaBitcoin className='icon'/>
+          <FaBitcoin className='icon' />
         </div>
       </IconContext.Provider>
     );
@@ -131,7 +120,7 @@ export default function Home() {
         value={{ color: '#716b94', size: '26px' }}
       >
         <div>
-          <FaEthereum className='icon'/>
+          <FaEthereum className='icon' />
         </div>
       </IconContext.Provider>
     );
@@ -142,7 +131,7 @@ export default function Home() {
         value={{ color: '#26A17B', size: '26px' }}
       >
         <div>
-          <SiTether className='icon'/>
+          <SiTether className='icon' />
         </div>
       </IconContext.Provider>
     );
@@ -150,10 +139,10 @@ export default function Home() {
   function SwitchIcon() {
     return (
       <IconContext.Provider
-        value={{ color: '#999999', size: '14px'}}
+        value={{ color: '#999999', size: '14px' }}
       >
         <div>
-          <FaArrowRightArrowLeft className='iconSwitch'/>
+          <FaArrowRightArrowLeft className='iconSwitch' />
         </div>
       </IconContext.Provider>
     );
@@ -162,18 +151,18 @@ export default function Home() {
   let coinNameLeft
   let coinNameRight
 
-  {(leftSide === 'BTC') ? coinNameLeft = 'Bitcoin = ' : (leftSide === 'ETH') ? coinNameLeft = 'Etherium = ' : (leftSide === 'USDT') ? coinNameLeft = 'Tether = ' : null}
-  {(rightSide === 'BTC') ? coinNameRight = 'Bitcoin' : (rightSide === 'ETH') ? coinNameRight = 'Etherium' : (rightSide === 'USDT') ? coinNameRight = 'Tether' : null}
+  { (leftSide === 'BTC') ? coinNameLeft = 'Bitcoin = ' : (leftSide === 'ETH') ? coinNameLeft = 'Etherium = ' : (leftSide === 'USDT') ? coinNameLeft = 'Tether = ' : null }
+  { (rightSide === 'BTC') ? coinNameRight = 'Bitcoin' : (rightSide === 'ETH') ? coinNameRight = 'Etherium' : (rightSide === 'USDT') ? coinNameRight = 'Tether' : null }
 
   let today
-  if (btc && eth) today = new Date().toLocaleString("ru-RU", {timeZone: "Europe/Moscow"}) + ' МСК'
+  if (btc && eth) today = new Date().toLocaleString("ru-RU", { timeZone: "Europe/Moscow" }) + ' МСК'
 
   return (
     <main className='main'>
 
       <div className='wrapper'>
         <input onChange={handleChange} value={numberLeft} name="left" type="number" min={0} max={1000000000} className='input inputLeft' placeholder="..."></input>
-    
+
         <select onChange={handleLeftSide} defaultValue="BTC" className='select'>
           <option value="BTC" defaultChecked>BTC</option>
           <option value="ETH">ETH</option>
@@ -185,7 +174,7 @@ export default function Home() {
       </div>
 
       <SwitchIcon />
-      
+
       <div className='wrapper'>
         <input onChange={handleChange} value={numberRight} name="right" type="number" min={0} max={1000000000} className='input inputRight' placeholder="..."></input>
 
@@ -205,9 +194,7 @@ export default function Home() {
             Данные носят ознакомительный характер {today}
           </p>
         </div>
-
       </div>
-
     </main>
   )
 }
